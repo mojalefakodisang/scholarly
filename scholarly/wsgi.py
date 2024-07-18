7e1fd96401a45fd443440c75ff244e25f409aa40
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-07-18 22:36:40 +0200

    [Update] - removed debug in WSGI.py

--- scholarly/wsgi.py
+++ scholarly/wsgi.py
@@ -1,9 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from django.conf import settings
 
-
-settings.DEBUG = False
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scholarly.settings')
 
 application = get_wsgi_application()
